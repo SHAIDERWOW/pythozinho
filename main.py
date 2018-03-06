7420c94b2faab43753ca63133b926929df07685a
vagner2k18 <36799806+vagner2k18@users.noreply.github.com>
2018-03-06 19:40:34 -0300

    first commit

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
-import asyncio
-import requests
-import io
-import aiohttp
+import secreto
+import os
 
 ''' VARIAVEIS'''
+
 bot = commands.Bot(command_prefix='?', description='Bem vindos Ao discord.')
 client = discord.Client()
+is_prod = os.environ.get('IS_HEROKU', None)
+if is_prod:
+    token = os.environ.get('TOKEN')
+else:
+    import secreto
+    token = secreto.token
+token = secreto.token
 user = discord.Member
-
-
 ''' EVENTOS DO BOT'''
 #MOSTRA NO TERMINAL ONDE TA LOGADO E MUDA STATUS DO BOT
 @bot.event
 async def on_ready():
-    print('Logged in as')
+    print('Logado em')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
     return await bot.change_presence(game=discord.Game(name='Programando'))
 
 
+
+
 #ENVIA UMA MENSAGEM PRIVADA AO ENTRAR NO SERVER
 @bot.event
 async def on_member_join(member):
     await bot.send_message(member,
-                           'Bem Vindo ao ' + member.server.name + ' ' + member.mention + '\n Leias as regras .')
+                           'Bem Vindo ao ' + member.server.name + ' ' + member.mention + '\n Estou aqui para ajudar ,digite ?help para ver so comandos .')
 
     await bot.add_roles(member)
 
@@ -69,14 +74,14 @@ async def ping(ctx):
 
 #MOSTRA MENSAGEM DE BOAS VINDAS
 @bot.command()
-async def ola():
-     await bot.say("Bem vindo ao discord de apredizado de programação")
+async def python():
+     await bot.say("Python é uma linguagem de programação criada por Guido van Rossum em 1991.\n Os objetivos do projeto da linguagem eram: produtividade e legibilidade.\n Em outras palavras, Python é uma linguagem que foi criada para produzir código bom e fácil de manter de maneira rápida.")
 
 
 #MOSTRA INFORMAÇÕES DO SERVIDOR
 @bot.command()
 async def info():
-    embedin = discord.Embed(title="Olá sou Asta kun", description="info do serve.", color=0xeee657)
+    embedin = discord.Embed(title="Olá sou pythozinho", description="convite e membros online.", color=0xeee657)
     embedin.add_field(name="Author", value="Vagner")
     embedin.add_field(name="Server count", value=f"{(len(bot.servers))}")
     embedin.add_field(name="Invite", value="https://discord.gg/vSFN3Zx")
@@ -88,15 +93,15 @@ async def info():
 #ENVIA UM GATINHO EM GIF
 @bot.command()
 async def cat():
-    await bot.say("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+    await bot.say("https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif")
 
 
 #EXIBE OS COMANDOS DO BOT
 bot.remove_command('help')
 @bot.command()
 async def help():
-    embedhelp = discord.Embed(title="Asta Kun", description="Olá eu fui criado em python, abaixo segue minha lista de comandos:", color=0xeee657)
-    embedhelp.add_field(name="?ola", value="Mensagem de boas vindas", inline=False)
+    embedhelp = discord.Embed(title="Pythozinho", description="Olá eu fui criado em python, abaixo segue minha lista de comandos:", color=0xeee657)
+    embedhelp.add_field(name="?python", value="Sobre o python", inline=False)
     embedhelp.add_field(name="?cat", value="Gatinho gif :3", inline=False)
     embedhelp.add_field(name="?info", value="Informaçoes", inline=False)
     embedhelp.add_field(name="?help", value="Ajuda", inline=False)
@@ -110,4 +115,5 @@ async def help():
 
 
 '''TOKEN PARA RODAR BOT'''
-bot.run('NDE0ODMwMzQzMTQ1NjUyMjI0.DX3Tqg.EOkA8JP10J5FXbUsnYdCREJ47-8')
+bot.run(token)
+
