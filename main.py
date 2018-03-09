d7906dc01a694463414fffdcfecff33d92195b72
vagner2k18 <36799806+vagner2k18@users.noreply.github.com>
2018-03-09 16:23:19 -0300

    new commands

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import os
+import time
+import random
 
 ''' VARIAVEIS'''
 
@@ -37,6 +39,28 @@ async def on_member_join(member):
 
 
 ''' COMANDOS DO BOT'''
+#ROLA O DADO
+@bot.command(pass_context=True, aliases=['dice'])
+async def dado(ctx):
+    choice  = random.randint(1,6)
+    embeddad = discord.Embed(title = 'Dado', description = ' Joguei o dado, o resultado é :   {}'.format(choice), colour = 0x1abc9c)
+    await bot.say(embed=embeddad)
+
+#JOGA A MOEDA
+@bot.command(pass_context=True, aliases=['coin'])
+async def moeda(ctx):
+    Coin = ["É CARA!", "É COROA!"]
+    choice = random.choice(Coin)
+    embedcon = discord.Embed(title='Moeda', description=choice, colour=0x1abc9c)
+    await bot.say(embed=embedcon)
+
+#ENVIA UM VIDEO ALEATORIO DO MEU CANAL
+@bot.command(pass_context=True)
+async def music(ctx):
+    song = ['https://www.youtube.com/watch?v=XzK7nCUCHEA', 'https://www.youtube.com/watch?v=G684IQwqodM', 'https://www.youtube.com/watch?v=uukforYWCkk', 'https://www.youtube.com/watch?v=3_29xfehVgY']
+    choice  = random.choice(song)
+    await bot.say('{}, Aqui está sua musica aleátoria.\n'.format(ctx.message.author.name) + choice)
+
 
 #GERA CONVITE
 @bot.command(pass_context=True)
@@ -64,18 +88,23 @@ async def myinfo(ctx):
     await bot.say(embed=embedusu)
 
 
-#COMANDO PING PONG *SEM MOSTRAR MS
-@bot.command(pass_context=True)
-async def ping(ctx):
-    await bot.say("Pong!")
+#COMANDO PING PONG 
+@bot.command(aliases=['p'])
+async def ping():
+    pingtime = time.time()
+    e = discord.Embed(title = 'Ok espere', color = 0x1abc9c)
+    pingus = await bot.say(embed=e)
+    ping = time.time() - pingtime
+    ping1 = discord.Embed(title = 'Pong!', description = ':ping_pong: Ping time - `%.01f seconds`' % ping, colour = 0x1abc9c)
+    await bot.edit_message(pingus, embed=ping1)
 
 
-#MOSTRA MENSAGEM DE BOAS VINDAS
+#FALA SOBRE O PYTHON
 @bot.command()
 async def python():
      await bot.say("Python é uma linguagem de programação criada por Guido van Rossum em 1991.\n Os objetivos do projeto da linguagem eram: produtividade e legibilidade.\n Em outras palavras, Python é uma linguagem que foi criada para produzir código bom e fácil de manter de maneira rápida.")
 
-#MOSTRA MENSAGEM DE BOAS VINDAS
+#FALA SOBRE O JAVA
 @bot.command()
 async def java():
      await bot.say("é uma linguagem de programação e plataforma computacional lançada pela primeira vez pela Sun Microsystems em 1995.\n Existem muitas aplicações e sites que não funcionarão, a menos que você tenha o Java instalado, e mais desses são criados todos os dias..")
@@ -107,6 +136,10 @@ async def help():
     embedhelp.add_field(name="?cat", value="Gatinho gif :3", inline=False)
     embedhelp.add_field(name="?info", value="Informaçoes", inline=False)
     embedhelp.add_field(name="?help", value="Ajuda", inline=False)
+    embedhelp.add_field(name="?music", value="Mostra um video aleátorio do meu canal/ obs: da like", inline=False)
+    embedhelp.add_field(name="?moeda", value="Joga uma moeda", inline=False)
+    embedhelp.add_field(name="?ping", value="Mostra o tempo de resposta entre o bot e o servirdor", inline=False)
+    embedhelp.add_field(name="?dado", value="Rola um dado de 6 lados", inline=False)
     embedhelp.add_field(name="?convite", value="Envia o convite do serve no seu privado", inline=False)
     embedhelp.add_field(name="?delete", value="Deleta mensagem passando a quantidade", inline=False)
     embedhelp.add_field(name="?myinfo", value="Mostra suas informações", inline=False)
